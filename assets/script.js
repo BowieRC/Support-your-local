c5cd8bccec2608be09c617e724650ca5eb6e99b5
Create-88 <141834742+Create-88@users.noreply.github.com>
2023-10-19 23:31:36 +1030

    add simple site-navigation

---
+++ assets/script.js
@@ -0,0 +1,39 @@
+var homeNavEl = $('#home-nav');
+var aboutNavEl = $('#about-nav');
+var contactNavEl = $('#contact-nav');
+
+
+//Event listener on nav items changes url to other page
+aboutNavEl.on('click', function () {
+    document.location.replace('./about.html');
+  });
+
+contactNavEl.on('click', function () {
+    document.location.replace('./contact.html');
+});
+
+//Click event causes reload
+homeNavEl.on('click', function() {
+    location.reload();
+});
+
+
+//Below is script relevant to Contact and About Pages
+var backNavEl = $('#back-nav');
+
+backNavEl.on('click', function () {
+    document.location.replace('./index.html');
+})
+
+//code for nav hamburger to toggle 
+$(document).ready(function() {
+
+    // Check for click events on the navbar burger icon
+    $(".navbar-burger").click(function() {
+  
+        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+        $(".navbar-burger").toggleClass("is-active");
+        $(".navbar-menu").toggleClass("is-active");
+  
+    });
+  });
