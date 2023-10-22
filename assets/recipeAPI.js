c02e0b0e1741ba742d03ff6aab2d73253fcf0575
BowieRC <rowlandscrispebowie@gmail.com>
2023-10-22 17:32:09 +0930

    re-work API search

--- assets/recipeAPI.js
+++ assets/recipeAPI.js
@@ -1,83 +1,152 @@
-var keywordSearchEl = $("#searchKeyword");
-var searchButtonEl = $("#enter");
-var containerEl = $(".container");
-var mod = '';
-var searchType = ["/search.php?s=", '/search.php?f=']
-var wrapperEl;
+var sortContainerEl = $("#sortByContainer");
+var sortOptionsEl = $("#sortOptions");
+var catOptionsEl = $("#catOptions");
+var areaOptionsEl = $("#areaOptions");
+var ingsOptionsEl = $("#ingrOptions");
+var filterOptionsEl = $('#fOptionsList');
 
-var hasChild = containerEl.wrapperEl;
+var requestURL = "HTTPS://www.themealdb.com/api/json/v1/1/";
+var getCat = "list.php?c=list";
+var getArea = "list.php?a=list";
+var getIngr = "list.php?i=list";
+var searchIng = "filter.php?i=";
+var searchCats = "filter.php?c=";
+var searchArea = "filter.php?a=";
 
-searchButtonEl.on("click", () => {
-    searchText = keywordSearchEl.val();
-    mod = "/search.php?s=" + searchText;
-    callAPI();
-});
-
-function callAPI(){
-    var requestUTL = "https://www.themealdb.com/api/json/v1/1/";
-    fetch(requestUTL + mod, {
+//When changing the Sort By dropdown
+sortOptionsEl.on("change", () => { 
+  if(sortOptionsEl.val() != "null"){
+  
+  var fOptions = document.getElementById("fOptionsList");  
+  while (fOptions.children[0]){
+      fOptions.remove(fOptions.children[0]);
+    }
+  filterBy();    
+  }
+})
+//When changing result of sort by
+filterOptionsEl.on("change", () => {
+  var searchParameter = filterOptionsEl.val();
+  // console.log(requestURL+searchIng+searchParameter.toLowerCase());
+  console.log(sortOptionsEl.val())
+  if(sortOptionsEl.val() == "Categories"){
+  fetch (requestURL+searchCats+searchParameter, {})
+    .then(function (response) {
+        return response.json();
     })
+.then(function(data) {
+    for(i=0; i<data.meals.length; i++){
+        console.log(data.meals[i].strMeal);      
+    }
+    console.log(data);
+    
+        })
+  }
+  if(sortOptionsEl === "Area"){
+    console.log("Area Search");
+  }
+  if(sortOptionsEl === "Ingredients"){
+    "Area Search"
+  }
+});
+  
+// chooses which option to filter by
+function filterBy(){
+    switch (sortOptionsEl.val()){
+    case ("Categories"): 
+        document.getElementById("filterOptions").classList.remove("hidden");
+        document.getElementById("filterOptionsLabel").textContent = "Categories";
+        getCats();
+      break;
+    case ("Area"): 
+        document.getElementById("filterOptions").classList.remove("hidden");
+        document.getElementById("filterOptionsLabel").textContent = "Areas";
+        
+        getAreas();
+      break;
+    case ("Ingredients"): 
+        document.getElementById("filterOptions").classList.remove("hidden");
+        document.getElementById("filterOptionsLabel").textContent = "Ingredients";
+        
+        getIngs();
+      break;
+    viewFilteredContent();
+  }
+}
+
+// if the categories option is selected
+function getCats() {   
+  fetch( requestURL + getCat, {})
     .then(function (response) {
         return response.json();
     })
     .then(function (data) {
-        displayMeals(data);
-
-        console.log(data);
-
-    })
-}
-
-function displayMeals(data) {
-    wrapperEl = $("<div>");
-    containerEl.append(wrapperEl);
-    if(data.meals != null){
         for(i=0; i<data.meals.length; i++){
-
-            var mealWrapperEl = $("<div>");
-            mealWrapperEl.attr('class', 'mealWrapper');
-            var imageWrapperEl = $('<div>');
-            imageWrapperEl.attr('class', 'imageWrapper');
-            imageWrapperEl.attr('style', 'background-image: url( "'+ data.meals[i].strMealThumb + '")');
-            var mealTitleEl = $('<h1>');
-            mealTitleEl.text(data.meals[i].strMeal);
-            var mealRegionEl = $('<h1>');
-            mealRegionEl.text(data.meals[i].strArea);
-            var mealCategoryEl = $('<h1>');
-            mealCategoryEl.text(data.meals[i].strCategory);
-
-            wrapperEl.append(mealWrapperEl);
-            mealWrapperEl.append(imageWrapperEl);
-            mealWrapperEl.append(mealTitleEl);
-            mealWrapperEl.append(mealRegionEl);
-            mealWrapperEl.append(mealCategoryEl);
-
-
-
-
-            // var mealWrapperEl = $("<img src='" + data.meals[i].strMealThumb + "'>");
-            // mealWrapperEl.attr("style", "background-image: url(" + data.meals[i].strMealThumb + ")");
-
-            // wrapperEl.append(mealWrapperEl);
-
-
-            // var mealNamesEl = $("<h1>");
-            // mealNamesEl.text(data.meals[i].strMeal);
-            // wrapperEl.append(mealNamesEl);
-            
-    
-            // console.log(data.meals[i].strMeal);
+          var catChoiceEl = $('<option>');
+          catChoiceEl.text(data.meals[i].strCategory);
+          catChoiceEl.attr("value", data.meals[i].strCategory);
+          filterOptionsEl.append(catChoiceEl);
         }
-    } else {
-        var mealNamesEl = $("<h1>");
-        mealNamesEl.text("Nothing called " + searchText);
-        console.log("Nada");
-        wrapperEl.append(mealNamesEl);
-    } 
-    
+  })
 }
 
-function displayRecipe() {
-    containerEl.remove(wrapperEl);
-    callAPI();
+// if the areas option is selected
+function getAreas(){   
+  fetch( requestURL + getArea, {})
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+    
+        for(i=0; i<data.meals.length; i++){
+        var areaChoiceEl = $('<option>');
+          areaChoiceEl.text(data.meals[i].strArea);
+          areaChoiceEl.attr("value", data.meals[i].strArea);
+          filterOptionsEl.append(areaChoiceEl);
+          
+        }
+  })
 }
+
+// if the ingredients option is selected
+function getIngs(){   
+  fetch( requestURL + getIngr, {})
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+    
+        for(i=0; i<data.meals.length; i++){
+        var ingsChoiceEl = $('<option>');
+         ingsChoiceEl.text(data.meals[i].strIngredient);
+          ingsChoiceEl.attr("value", data.meals[i].strIngredient);
+          filterOptionsEl.append(ingsChoiceEl);
+        }
+  })
+}
+
+function displaySearch(data) {
+  var sortContainer = $("<section>");
+  
+  for(i=0; i<data.meals.length; i++){  
+  var sortWrapper = $("<div>");
+  var imageWrapper = $("<div>");
+  var sortedTitle = $("<h1>");
+  var sortedDescription = $("<p>");
+  
+  sortContainer.attr("id", "sort-container");
+  sortWrapper.attr("class", "sortWrapper");
+  imageWrapper.attr("class", "imageWrapper");
+  sortedTitle.attr("class", "sortedTitle");
+  sortedDescription.attr("class", "sortedDescription");
+  
+  sortedTitle.text(data.meals[i].strMeals);
+  sortedDescription.text(data.meals[i].strMeals);
+    
+  sortContainer.append(sortWrapper);
+  sortWrapper.append(sortedTitle)
+  imageWrapper
+  }
+  
+}
+
