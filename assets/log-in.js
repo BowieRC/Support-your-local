ea53d86a0e1570749e2e61833664451e7cadbbac
Create-88 <141834742+Create-88@users.noreply.github.com>
2023-10-27 01:33:55 +1030

    save sign up data to local storage

--- assets/log-in.js
+++ assets/log-in.js
@@ -6,20 +6,22 @@
 
 document.addEventListener('DOMContentLoaded', () => {
     
-    var signupModalEl = $("#modal-sign-up")
+    // var signupModalEl = $("#modal-sign-up");
+    var signupModalEl = document.getElementById('modal-sign-up');
+    console.log(signupModalEl, 'signupModalEl');
     var signUpButton = document.querySelector("#signup-button");
-    // var openModal = $(signupModalEl).show();
 
-    //
-    function checkOpen() {
-        $(signupModalEl).hasClass('is-active');
-            return true
-        
-    }
+    
 
-    if (checkOpen()) {
-        console.log('checked - modal open');
-        
+   
+    //successful code
+    // if ($("#modal-sign-up").hasClass("is-active")) {
+    //     console.log('sign up is active');
+
+    // } else {
+    //     console.log('it is not open');
+    // }
+    
         signUpButton.addEventListener("click", function(event) {
             event.preventDefault;
             
@@ -44,41 +46,57 @@ document.addEventListener('DOMContentLoaded', () => {
             var password = $(passwordInput).val();
             var confirm = $(confirmPassword).val();
             
-            var inputRootEl = $('#signup-body');
+            var agreeCheckbox = document.querySelector('#agree-terms');
+
+            if(!firstName || !lastName || !email || !password || !confirm || !agreeCheckbox.checked) {
+                console.log('Please check that all fields are complete.');
+                return;
+            } else if (confirmPassword.value !== passwordInput.value) {
+                console.log('Passwords do not match');
+                return;
+            } else {
+                localStorage.setItem("user", JSON.stringify(user));
+                var userCredentials = localStorage.getItem("user");
+                console.log(JSON.parse(userCredentials), "userCredentials");
+            }
+
+
+
+
+
+            // var inputRootEl = $('#signup-body');
 
      
-            //This returns all input fields within the signup section
-            var inputEls = inputRootEl.children().children().children('input').val();
+            // //This returns all input fields within the signup section
+            // var inputEls = inputRootEl.children().children().children('input').val();
             
-            //Not working yet - this function currently writes the content of only 1 input field to the console. I want it to check whether it is empty. 
-            //Only returning the first input field
-            console.log(inputEls);
+            // //Not working yet - this function currently writes the content of only 1 input field to the console. I want it to check whether it is empty. 
+            // //Only returning the first input field
+            // console.log(inputEls);
             
             //function works but the inputEls only has on field
-            function checkInputEls() {
-                for (var i=0; i < inputEls.length; i++) {
+            // function checkInputEls() {
+            //     for (var i=0; i < inputEls.length; i++) {
                     
-                    console.log(inputEls.length);
-                    console.log(inputEls[i]);
+            //         console.log(inputEls.length);
+            //         console.log(inputEls[i]);
 
-                     if (!inputEls[i]) {
-                         console.log([i] + " is empty");
+            //          if (!inputEls[i]) {
+            //              console.log([i] + " is empty");
                                 
-                    } else if (inputEls[i]) {
-                        console.log([i] + " says " + inputEls);
-                    }
-                };
-            };
-
-            checkInputEls();
+            //         } else if (inputEls[i]) {
+            //             console.log([i] + " says " + inputEls);
+            //         }
+            //     };
+            // };
 
             //confirm password must match password in order to progress. 
             
             //Sign up button only available when all fields are full and when checkbox ticked.
             //Added these classes to index.html <input type="checkbox" id="agreeTerms" name="agree" value="agreement">
 
-            var agreeCheckbox = document.querySelector('#agreeTerms');
-            
+           
+                      
 
             //SUCCESSFUL CODE
             // if (!agreeCheckbox.checked || confirmPassword.value !== passwordInput.value) {
@@ -89,30 +107,20 @@ document.addEventListener('DOMContentLoaded', () => {
             //             console.log(JSON.parse(userCredentials), "userCredentials");  
             // }
 
-            if (!agreeCheckbox.checked || confirmPassword.value !== passwordInput.value) {
-                console.log("success");
-             
-            } else {
-                       localStorage.setItem("user", JSON.stringify(user));
-                        var userCredentials = localStorage.getItem("user");
-                        console.log(JSON.parse(userCredentials), "userCredentials");  
-            }
+            // // Clear input fields
+            // $('input[type="text"]').val('');
+            // $('input[type="email"]').val('');
+            // $('input[type="checkbox"]').prop('checked', false);
 
-            // Clear input fields
-            $('input[type="text"]').val('');
-            $('input[type="email"]').val('');
-            $('input[type="checkbox"]').prop('checked', false);
 
         });
        
 
 
-    };
-
-});
+    });
 
 $(document).ready(function() {
     $("#terms-link").click(function() {
         window.open("ts&cs.html", "_blank");
     });
-})
+});
