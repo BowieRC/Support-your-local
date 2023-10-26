a5af19a8567c52097b602f525339b505c866a8cc
Create-88 <141834742+Create-88@users.noreply.github.com>
2023-10-26 14:24:42 +1030

    progress towards checking if empty fields

--- assets/log-in.js
+++ assets/log-in.js
@@ -15,12 +15,14 @@ document.addEventListener('DOMContentLoaded', () => {
         
         signUpButton.addEventListener("click", function(event) {
             event.preventDefault;
+            
 
             var firstNameInput = document.querySelector('input[name="first-name"]');
             var lastNameInput = document.querySelector('input[name="last-name"]');
             var emailInput = document.querySelector('input[name="email"]');
             var passwordInput = document.querySelector('input[name="password"]');
-            var confirmPassword = document.querySelector('input[name="')
+            
+            var confirmPassword = document.querySelector('input[name="confirm-password"]')
 
             var user = {
                 firstName: firstNameInput.value.trim(),
@@ -28,7 +30,55 @@ document.addEventListener('DOMContentLoaded', () => {
                 email: emailInput.value.trim(),
                 password: passwordInput.value.trim(),
             };
+            
+            var firstName = $(firstNameInput).val();
+            var lastName = $(lastNameInput).val();
+            var email = $(emailInput).val();
+            var password = $(passwordInput).val();
+            var confirm = $(confirmPassword).val();
+            
+            var inputRootEl = $('#signup-body');
 
+     
+            //This returns all input fields within the signup section
+            var inputEls = inputRootEl.children().children().children('input').val();
+            
+            //Not working yet - this function currently writes the content of only 1 input field to the console. I want it to check whether it is empty. 
+            //Only returning the first input field
+            console.log(inputEls);
+            
+            //function works but the inputEls only has on field
+            function checkInputEls() {
+                for (var i=0; i < inputEls.length; i++) {
+                    
+                    console.log(inputEls.length);
+                    console.log(inputEls[i]);
+
+                     if (!inputEls[i]) {
+                         console.log([i] + " is empty");
+                                
+                    } else if (inputEls[i]) {
+                        console.log([i] + " says " + inputEls);
+                    }
+                };
+            };
+
+            checkInputEls();
+                    
+                                                   
+    
+            
+         
+
+           
+                
+            
+           
+
+
+
+         
+        
             //confirm password must match password in order to progress. 
             
             //Sign up button only available when all fields are full and when checkbox ticked.
@@ -37,25 +87,28 @@ document.addEventListener('DOMContentLoaded', () => {
             var agreeCheckbox = document.querySelector('#agreeTerms');
             
 
-            // agreeCheckbox.addEventListener("change", () => {
-            //     if(!agreeCheckbox.checked || $('.class') === "") {
-            //         //add appending text next
-            //         console.log("Please check you've filled in the form correctly");
-            //         //working but only after sign up button pressed
-            //     } else {
-            //         localStorage.setItem("user", JSON.stringify(user));
-            //         var userCredentials = localStorage.getItem("user");
-            //         console.log(JSON.parse(userCredentials), "userCredentials");
-            //     }
+            //SUCCESSFUL CODE
+            // if (!agreeCheckbox.checked || confirmPassword.value !== passwordInput.value) {
+            //     console.log("Please check you have filled in the form correctly.");
+            // } else {
+            //            localStorage.setItem("user", JSON.stringify(user));
+            //             var userCredentials = localStorage.getItem("user");
+            //             console.log(JSON.parse(userCredentials), "userCredentials");  
+            // }
 
-            // });
-            if (!agreeCheckbox.checked) {
-                console.log("Please check you have filled in the form correctly.");
+            if (!agreeCheckbox.checked || confirmPassword.value !== passwordInput.value) {
+                console.log("success");
+             
             } else {
                        localStorage.setItem("user", JSON.stringify(user));
                         var userCredentials = localStorage.getItem("user");
                         console.log(JSON.parse(userCredentials), "userCredentials");  
             }
+
+            // Clear input fields
+            $('input[type="text"]').val('');
+            $('input[type="email"]').val('');
+            $('input[type="checkbox"]').prop('checked', false);
 
         });
 
