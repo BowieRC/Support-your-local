d0d3d8106b81d1a8696cbe6e5ed6286e8e522140
Benji Cullen <141322492+BenjiCullen@users.noreply.github.com>
2023-10-29 23:00:07 +1030

    adding to maps

--- assets/google-maps.js
+++ assets/google-maps.js
@@ -27,34 +27,130 @@ function initMap() {
     // Browser doesn't support Geolocation
     window.alert("Geolocation is not supported by this browser.");
   }
-  
-}
- 
+  const script = document.createElement("script");
+  // This example uses a local copy of the GeoJSON stored at
+  // http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonp
+  script.src =
+    "https://developers.google.com/maps/documentation/javascript/examples/json/earthquake_GeoJSONP.js";
+  document.getElementsByTagName("head")[0].appendChild(script);
 
-//distance between locations
-//   const directionsRenderer = new google.maps.DirectionsRenderer();
-//   const directionsService = new google.maps.DirectionsService();
 
-infowindow = new google.maps.InfoWindow();
- map = new google.maps.Map(document.getElementById("map"), {
-    center: adelaide,
-   zoom: 15,                                                          
+  //This is the start of the code for finding the grocery store locations
+  function initAutocomplete() {
+  const map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: -33.8688, lng: 151.2195 },
+    zoom: 13,
+    mapTypeId: "roadmap",
+  });
+  // Create the search box and link it to the UI element.
+  const input = document.getElementById("pac-input");
+  const searchBox = new google.maps.places.SearchBox(input);
+
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+  // Bias the SearchBox results towards current map's viewport.
+  map.addListener("bounds_changed", () => {
+    searchBox.setBounds(map.getBounds());
   });
 
-  directionsRenderer.setMap(map);
-  directionsRenderer.setPanel(document.getElementById("sidebar"));
-  const control = document.getElementById("floating-panel");
+  let markers = [];
 
-  map.controls[google.maps.ControlPosition.TOP_CENTER].push(control);
+  // Listen for the event fired when the user selects a prediction and retrieve
+  // more details for that place.
+  searchBox.addListener("places_changed", () => {
+    const places = searchBox.getPlaces();
 
-  const onChangeHandler = function () {
-    calculateAndDisplayRoute(directionsService, directionsRenderer);
-  };
-//both the origin and destination inputs below are custom and refrenced from the HTML. were previously start and end of the input fields respectfully.
-  document.getElementById("origin-input").addEventListener("change", onChangeHandler);
-  document.getElementById("destination-input").addEventListener("change", onChangeHandler);
+    if (places.length == 0) {
+      return;
+    }
 
-  // This is for the route
+    // Clear out the old markers.
+    markers.forEach((marker) => {
+      marker.setMap(null);
+    });
+    markers = [];
+
+    // For each place, get the icon, name and location.
+    const bounds = new google.maps.LatLngBounds();
+
+    places.forEach((place) => {
+      if (!place.geometry || !place.geometry.location) {
+        console.log("Returned place contains no geometry");
+        return;
+      }
+
+      const icon = {
+        url: place.icon,
+        size: new google.maps.Size(71, 71),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(17, 34),
+        scaledSize: new google.maps.Size(25, 25),
+      };
+
+      // Create a marker for each place.
+      markers.push(
+        new google.maps.Marker({
+          map,
+          icon,
+          title: place.name,
+          position: place.geometry.location,
+        }),
+      );
+      if (place.geometry.viewport) {
+        // Only geocodes have viewport.
+        bounds.union(place.geometry.viewport);
+      } else {
+        bounds.extend(place.geometry.location);
+      }
+    });
+    map.fitBounds(bounds);
+  });
+}
+
+
+  //This is for finding the destinations of grocery stores
+  // const request = {
+  //   query: "",
+  //   fields: ["name", "geometry"],
+  // };
+  // service = new google.maps.places.PlacesService(map);
+  // service.findPlaceFromQuery(request, (results, status) => {
+  //   if (status === google.maps.places.PlacesServiceStatus.OK && results) {
+  //     for (let i = 0; i < results.length; i++) {
+  //       createMarker(results[i]);
+  //     }
+
+  //     map.setCenter(results[0].geometry.location);
+  //   }
+  // });
+}
+
+function createMarker(place) {
+  if (!place.geometry || !place.geometry.location) return;
+
+  const marker = new google.maps.Marker({
+    map,
+    position: place.geometry.location,
+  });
+
+  google.maps.event.addListener(marker, "click", () => {
+    infowindow.setContent(place.name || "");
+    infowindow.open(map);
+  });
+}
+
+// Loop through the results array and place a marker for each
+// set of coordinates.
+const eqfeed_callback = function (results) {
+  for (let i = 0; i < results.features.length; i++) {
+    const coords = results.features[i].geometry.coordinates;
+    const latLng = new google.maps.LatLng(coords[1], coords[0]);
+
+    new google.maps.Marker({
+      position: latLng,
+      map: map,
+    });
+  }
+}  // This is for the route
 
 function calculateAndDisplayRoute(directionsService, directionsRenderer) {
   const start = document.getElementById("origin-input").value;
@@ -71,29 +167,7 @@ function calculateAndDisplayRoute(directionsService, directionsRenderer) {
       directionsRenderer.setDirections(response);
     })
     .catch((e) => window.alert("Directions request failed due to " + status));
-}
-
-  // This is for the route
-  new AutocompleteDirectionsHandler(map);
-//search field for places, need to add a search box on user side
-  const request = {
-    query: "coles",
-    fields: ["name", "geometry"],
-  };
-
-  service = new google.maps.places.PlacesService(map);
-  service.findPlaceFromQuery(request, (results, status) => {
-    if (status === google.maps.places.PlacesServiceStatus.OK && results) {
-      for (let i = 0; i < results.length; i++) {
-        createMarker(results[i]);
-      }
-
-      map.setCenter(results[0].geometry.location);
-    }
-});
-  //continue on for route
-
-class AutocompleteDirectionsHandler {
+}class AutocompleteDirectionsHandler {
   map;
   originPlaceId;
   destinationPlaceId;
@@ -122,26 +196,6 @@ class AutocompleteDirectionsHandler {
       destinationInput,
       { fields: ["place_id"] },
     );
-
-    this.setupClickListener(
-      "changemode-walking",
-      google.maps.TravelMode.WALKING,
-    );
-    this.setupClickListener(
-      "changemode-transit",
-      google.maps.TravelMode.TRANSIT,
-    );
-    this.setupClickListener(
-      "changemode-driving",
-      google.maps.TravelMode.DRIVING,
-    );
-    this.setupPlaceChangedListener(originAutocomplete, "ORIG");
-    this.setupPlaceChangedListener(destinationAutocomplete, "DEST");
-    this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(originInput);
-    this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(
-      destinationInput,
-    );
-    this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(modeSelector);
   }
   // Sets a listener on a radio button to change the filter type on Places
   // Autocomplete.
@@ -153,50 +207,7 @@ class AutocompleteDirectionsHandler {
       this.route();
     });
   }
-  setupPlaceChangedListener(autocomplete, mode) {
-    autocomplete.bindTo("bounds", this.map);
-    autocomplete.addListener("place_changed", () => {
-      const place = autocomplete.getPlace();
-
-      if (!place.place_id) {
-        window.alert("Please select an option from the dropdown list.");
-        return;
-      }
-
-      if (mode === "ORIG") {
-        this.originPlaceId = place.place_id;
-      } else {
-        this.destinationPlaceId = place.place_id;
-      }
-
-      this.route();
-    });
-  }
-  route() {
-    if (!this.originPlaceId || !this.destinationPlaceId) {
-      return;
-    }
-
-    const me = this;
-
-    this.directionsService.route(
-      {
-        origin: { placeId: this.originPlaceId },
-        destination: { placeId: this.destinationPlaceId },
-        travelMode: this.travelMode,
-      },
-      (response, status) => {
-        if (status === "OK") {
-          me.directionsRenderer.setDirections(response);
-        } else {
-          window.alert("Directions request failed due to " + status);
-        }
-      },
-    );
-  }
-}
-
-// markers are created here
+}// markers are created here
 function createMarker(place) {
   if (!place.geometry || !place.geometry.location) return;
 
@@ -209,6 +220,9 @@ function createMarker(place) {
     infowindow.setContent(place.name || "");
     infowindow.open(map);
   });
+  const script = document.createElement("script");
 
-  }
+}
 window.initMap = initMap;
+window.eqfeed_callback = eqfeed_callback;
+window.initAutocomplete = initAutocomplete;
