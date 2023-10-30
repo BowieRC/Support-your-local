01bb43aebcfb92d750192e5cabe2a5a7e8fef3fe
Benji Cullen <141322492+BenjiCullen@users.noreply.github.com>
2023-10-30 14:24:39 +1030

    addition of store locations and fixed geolocation

--- assets/google-maps.js
+++ assets/google-maps.js
@@ -5,8 +5,6 @@ let service;
 let infowindow;
 
 function initMap() {
- // const adelaide = new google.maps.LatLng(-34.928, 138.599);
-  // find current location
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition((position) => {
       const pos = {
@@ -27,186 +25,16 @@ function initMap() {
     // Browser doesn't support Geolocation
     window.alert("Geolocation is not supported by this browser.");
   }
+  //this shows the location of stores close to the user location
   const script = document.createElement("script");
-  // This example uses a local copy of the GeoJSON stored at
-  // http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonp
+  // This example uses a local copy of the GeoJSON stored at google.com/maps/api/geocode/json.
   script.src =
-    "https://developers.google.com/maps/documentation/javascript/examples/json/earthquake_GeoJSONP.js";
+    "https://maps.googleapis.com/maps/api/geocode/json?place_id=ChIJVSZzVR8FdkgRTyQkxxLQmVU&key=AIzaSyCtBPYMqlBJKHDOSjwGdjw7BcKgB1BQWlA&solution_channel=GMP_guides_productlocator_v1_a";
   document.getElementsByTagName("head")[0].appendChild(script);
-
-
-  //This is the start of the code for finding the grocery store locations
-  function initAutocomplete() {
-  const map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -33.8688, lng: 151.2195 },
-    zoom: 13,
-    mapTypeId: "roadmap",
-  });
-  // Create the search box and link it to the UI element.
-  const input = document.getElementById("pac-input");
-  const searchBox = new google.maps.places.SearchBox(input);
-
-  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-  // Bias the SearchBox results towards current map's viewport.
-  map.addListener("bounds_changed", () => {
-    searchBox.setBounds(map.getBounds());
-  });
-
-  let markers = [];
-
-  // Listen for the event fired when the user selects a prediction and retrieve
-  // more details for that place.
-  searchBox.addListener("places_changed", () => {
-    const places = searchBox.getPlaces();
-
-    if (places.length == 0) {
-      return;
-    }
-
-    // Clear out the old markers.
-    markers.forEach((marker) => {
-      marker.setMap(null);
-    });
-    markers = [];
-
-    // For each place, get the icon, name and location.
-    const bounds = new google.maps.LatLngBounds();
-
-    places.forEach((place) => {
-      if (!place.geometry || !place.geometry.location) {
-        console.log("Returned place contains no geometry");
-        return;
-      }
-
-      const icon = {
-        url: place.icon,
-        size: new google.maps.Size(71, 71),
-        origin: new google.maps.Point(0, 0),
-        anchor: new google.maps.Point(17, 34),
-        scaledSize: new google.maps.Size(25, 25),
-      };
-
-      // Create a marker for each place.
-      markers.push(
-        new google.maps.Marker({
-          map,
-          icon,
-          title: place.name,
-          position: place.geometry.location,
-        }),
-      );
-      if (place.geometry.viewport) {
-        // Only geocodes have viewport.
-        bounds.union(place.geometry.viewport);
-      } else {
-        bounds.extend(place.geometry.location);
-      }
-    });
-    map.fitBounds(bounds);
-  });
 }
-
-
-  //This is for finding the destinations of grocery stores
-  // const request = {
-  //   query: "",
-  //   fields: ["name", "geometry"],
-  // };
-  // service = new google.maps.places.PlacesService(map);
-  // service.findPlaceFromQuery(request, (results, status) => {
-  //   if (status === google.maps.places.PlacesServiceStatus.OK && results) {
-  //     for (let i = 0; i < results.length; i++) {
-  //       createMarker(results[i]);
-  //     }
-
-  //     map.setCenter(results[0].geometry.location);
-  //   }
-  // });
-}
-
-function createMarker(place) {
-  if (!place.geometry || !place.geometry.location) return;
-
-  const marker = new google.maps.Marker({
-    map,
-    position: place.geometry.location,
-  });
-
-  google.maps.event.addListener(marker, "click", () => {
-    infowindow.setContent(place.name || "");
-    infowindow.open(map);
-  });
-}
-
-// Loop through the results array and place a marker for each
-// set of coordinates.
-const eqfeed_callback = function (results) {
-  for (let i = 0; i < results.features.length; i++) {
-    const coords = results.features[i].geometry.coordinates;
-    const latLng = new google.maps.LatLng(coords[1], coords[0]);
-
-    new google.maps.Marker({
-      position: latLng,
-      map: map,
-    });
-  }
-}  // This is for the route
-
 function calculateAndDisplayRoute(directionsService, directionsRenderer) {
   const start = document.getElementById("origin-input").value;
   const end = document.getElementById("destination-input").value;
-
- //These have been left unchanged for now.
-  directionsService
-    .route({
-      origin: start,
-      destination: end,
-      travelMode: google.maps.TravelMode.DRIVING,
-    })
-    .then((response) => {
-      directionsRenderer.setDirections(response);
-    })
-    .catch((e) => window.alert("Directions request failed due to " + status));
-}class AutocompleteDirectionsHandler {
-  map;
-  originPlaceId;
-  destinationPlaceId;
-  travelMode;
-  directionsService;
-  directionsRenderer;
-  constructor(map) {
-    this.map = map;
-    this.originPlaceId = "";
-    this.destinationPlaceId = "";
-    this.travelMode = google.maps.TravelMode.WALKING;
-    this.directionsService = new google.maps.DirectionsService();
-    this.directionsRenderer = new google.maps.DirectionsRenderer();
-    this.directionsRenderer.setMap(map);
-
-    const originInput = document.getElementById("origin-input");
-    const destinationInput = document.getElementById("destination-input");
-    const modeSelector = document.getElementById("mode-selector");
-    // Specify just the place data fields that you need.
-    const originAutocomplete = new google.maps.places.Autocomplete(
-      originInput,
-      { fields: ["place_id"] },
-    );
-    // Specify just the place data fields that you need.
-    const destinationAutocomplete = new google.maps.places.Autocomplete(
-      destinationInput,
-      { fields: ["place_id"] },
-    );
-  }
-  // Sets a listener on a radio button to change the filter type on Places
-  // Autocomplete.
-  setupClickListener(id, mode) {
-    const radioButton = document.getElementById(id);
-
-    radioButton.addEventListener("click", () => {
-      this.travelMode = mode;
-      this.route();
-    });
-  }
 }// markers are created here
 function createMarker(place) {
   if (!place.geometry || !place.geometry.location) return;
